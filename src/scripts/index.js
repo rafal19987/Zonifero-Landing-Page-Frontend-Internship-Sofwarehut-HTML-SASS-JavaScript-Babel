80373b6eb94463c660046708a5c0164751a5609e
Rafixx <rafal987.1@o2.pl>
2023-09-04 14:54:25 +0200

    feat: start creating header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,21 @@
 import '../styles/main.scss';
+
+// bottom menu select section effect
+const bottomMenuItems = document.querySelectorAll(
+  '.bottom-header__menu-list-item'
+);
+
+// initial selected 'Overview' section
+bottomMenuItems[0].classList.add('bottom-header__menu-list-item--active');
+
+bottomMenuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', (e) => toggleActiveClass(e));
+});
+
+const toggleActiveClass = (e) => {
+  bottomMenuItems.forEach((item) => {
+    item.classList.remove('bottom-header__menu-list-item--active');
+  });
+
+  e.target.classList.add('bottom-header__menu-list-item--active');
+};
