55b1fb613831462c31849034a9ab9ff08f19091e
Rafixx <rafal987.1@o2.pl>
2023-09-05 11:26:08 +0200

    import new features and assets to main js script

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,19 @@
 import '../styles/index.scss';
 
-import logoImg from '../assets/logo.png';
-import globe from '../assets/globe.png';
+// assets
+// -  for header
+import '../assets/logo.png';
+import '../assets/zonifero_logo.png';
+import '../assets/globe.png';
+import '../assets/arrow_down_icon.svg';
+
+// - for overview section
+import '../assets/desks.png';
+import '../assets/google_store_badge.png';
+import '../assets/apple_store_badge.png';
+import '../assets/hand.png';
+
+// features
 import './features/scrollToSelectedSection';
 import './features/openMobileMenu';
+import './features/selectPageLang';
