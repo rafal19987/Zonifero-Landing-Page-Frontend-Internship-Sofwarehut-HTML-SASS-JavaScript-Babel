f8bcc4f89554fa323df4ae33ff37addabbd2337d
Rafixx <rafal987.1@o2.pl>
2023-09-05 08:49:09 +0200

    refactor: rewrite index.js move features into separate files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,6 @@
-import '../styles/main.scss';
+import '../styles/index.scss';
 
-// bottom menu select section effect
-const bottomMenuItems = document.querySelectorAll(
-  '.bottom-header__menu-list-item'
-);
-
-// initial selected 'Overview' section
-bottomMenuItems[0].classList.add('bottom-header__menu-list-item--active');
-
-bottomMenuItems.forEach((menuItem) => {
-  menuItem.addEventListener('click', (e) => toggleActiveClass(e));
-});
-
-const toggleActiveClass = (e) => {
-  bottomMenuItems.forEach((item) => {
-    item.classList.remove('bottom-header__menu-list-item--active');
-  });
-
-  e.target.classList.add('bottom-header__menu-list-item--active');
-};
+import logoImg from '../assets/logo.png';
+import globe from '../assets/globe.png';
+import './features/scrollToSelectedSection';
+import './features/openMobileMenu';
