5a166fd45bc45140099c4933535d26d4c9ecbac5
Rafixx <rafal987.1@o2.pl>
2023-09-06 08:07:59 +0200

    feat: create array with articles for slider in overview page

---
+++ src/scripts/consts/sliderArticles.js
@@ -0,0 +1,27 @@
+export const sliderArticles = [
+  {
+    headline: 'Use your time and space smarter',
+    description:
+      'Never again run out of coffee or other supplies. Create an efficient hive for yout busy bees.',
+  },
+  {
+    headline: 'Never stress about me',
+    description:
+      'Zonifero will measure how you use the rooms so that you always have enough rooms available',
+  },
+  {
+    headline: 'Use your time and space smarter',
+    description:
+      'Never again run out of coffee or other supplies. Create an efficient hive for yout busy bees.',
+  },
+  {
+    headline: 'Never stress about me',
+    description:
+      'Zonifero will measure how you use the rooms so that you always have enough rooms available',
+  },
+  {
+    headline: 'Use your time and space smarter',
+    description:
+      'Never again run out of coffee or other supplies. Create an efficient hive for yout busy bees.',
+  },
+];
