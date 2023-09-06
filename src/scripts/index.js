64462198e441265bfd8204ce6fb350197a4e6330
Rafixx <rafal987.1@o2.pl>
2023-09-06 14:22:56 +0200

    assets: import assets for feature page in index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,6 +14,12 @@ import '../assets/apple_store_badge.png';
 import '../assets/hand.png';
 import '../assets/desk_icon.png';
 
+// - for features section
+import '../assets/Layer 78.png';
+import '../assets/mob_features_bg.png';
+import '../assets/mob_mobile.png';
+import '../assets/Silver.png';
+
 // features
 import './features/scrollToSelectedSection';
 import './features/openMobileMenu';
