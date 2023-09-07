f1d841e578c1f4d41f3253f65c9f865b2483cf24
Rafixx <rafal987.1@o2.pl>
2023-09-07 14:01:20 +0200

    feat: create Specs section for mobile devices

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -20,8 +20,23 @@ import '../assets/mob_features_bg.png';
 import '../assets/mob_mobile.png';
 import '../assets/Silver.png';
 
+// - for specs section
+import '../assets/mob_room_booking.png';
+import '../assets/Space_Gray.png';
+import '../assets/mob_czujka.png';
+import '../assets/logo_nsg.png';
+import '../assets/Layer 504.png';
+import '../assets/mob_devices copy.png';
+import '../assets/devices.png';
+import '../assets/mob_starter_kit.png';
+import '../assets/printer.png';
+import '../assets/Shadow_printer.png';
+import '../assets/Lobby Starter Kit.png';
+import '../assets/uchwyt.png';
+
 // features
 import './features/scrollToSelectedSection';
 import './features/openMobileMenu';
 import './features/selectPageLang';
 import './features/fetchAllArticlesForSlider';
+import './features/toggleAcordeonActiveField';
