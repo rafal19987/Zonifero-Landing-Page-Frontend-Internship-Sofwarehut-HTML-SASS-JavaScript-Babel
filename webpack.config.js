8f6600f1c345c9441fce3d1bf33d5906a3c1a375
Rafixx <rafal987.1@o2.pl>
2023-09-08 09:49:43 +0200

    chore: add html-loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,64 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        bundle: path.resolve(__dirname, 'src/scripts/index.js'),
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/scripts/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-        clean: true,
-        assetModuleFilename: '[name][ext]'
-    },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist')
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif|svg|ico)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
             },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.(png|jpeg|jpg|gif|svg)$/i,
-                type: 'asset/resource'
-            }
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            title: 'Chuck Norris Jokes',
-            filename: 'index.html',
-            template: path.resolve(__dirname, 'src/index.html')
-        })
-    ]
-}
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Chuck Norris Jokes',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+  ],
+};
